cbbc70009dccd73bbf6086844949959030d44397
Zsukan Vladut <flaviu.zsukan@fortech.ro>
2021-10-06 14:00:54 +0300

    ADD class1.js and index.html

---
+++ class1.js
@@ -0,0 +1,13 @@
+class Vehicle {
+    constructor(model,color){
+        this.model = model;
+        this.color = color;
+    }
+
+    getColor() {
+        return `Color of the vehicle: ${this.color} `
+    }
+}
+
+let bmw = new Vehicle("BMW X3", "Blue");
+console.log(bmw.getColor());
